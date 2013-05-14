7e8d4b6b6b6f391fecd98dbd8aa0fba4d8c10f20
Dan McAulay <dmcaulay@gmail.com>
2013-05-14 16:38:32 -0700

    Revert "adding more infomation to exceptions"
    This reverts commit 8b84e02877df670a77ee4d6df74232cae2e5db81.

--- lib/tumblr/request.rb
+++ lib/tumblr/request.rb
@@ -33,19 +33,19 @@ module Tumblr
       response = connection.request(method, path, *params)
       case response.code.to_i
       when 400
-        raise Tumblr::Error::BadRequestError.new(response.inspect)
+        raise Tumblr::Error::BadRequestError.new(response.body)
       when 401
-        raise Tumblr::Error::UnauthorizedError.new(response.inspect)
+        raise Tumblr::Error::UnauthorizedError.new(response.body)
       when 403
-        raise Tumblr::Error::ForbiddenError.new(response.inspect)
+        raise Tumblr::Error::ForbiddenError.new(response.body)
       when 404
-        raise Tumblr::Error::NotFoundError.new(response.inspect)
+        raise Tumblr::Error::NotFoundError.new(response.body)
       when 500
-        raise Tumblr::Error::InternalServerError.new(response.inspect)
+        raise Tumblr::Error::InternalServerError.new(response.body)
       when 502
-        raise Tumblr::Error::BadGatewayError.new(response.inspect)
+        raise Tumblr::Error::BadGatewayError.new(response.body)
       when 503
-        raise Tumblr::Error::ServiceUnavailableError.new(response.inspect)
+        raise Tumblr::Error::ServiceUnavailableError.new(response.body)
       end
 
       response["Content-Type"] =~ /application\/json/ ? Hashie::Mash.new(JSON::parse(response.body)) : response.body
